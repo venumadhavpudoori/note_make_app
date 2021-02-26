6f1d5a2e00b6c354e3ca09f1f236632f851e21a1
Venumadhav Pudoori <venumadhavpudoori@gmail.com>
2021-02-26 15:32:42 +0530

    fixed bugs

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState } from 'react'
 import './NoteList.css';
 import NoteMenuAction from "./NoteMenuAction";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
